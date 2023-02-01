011bddd586b6ba4256cab555e287a45868cf65f8
Ebrahim Abd EL Razik <94077806+ebrahim-0@users.noreply.github.com>
2023-02-02 01:11:36 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,40 @@
+const input = document.querySelector("form div input");
+const submit = document.querySelector("form div button");
+const ul = document.querySelector("ul");
+const lis = document.querySelectorAll("ul li");
+const del = document.querySelector(".delete");
+const filter = document.querySelector(".filter");
+
+submit.addEventListener("click", (e) => {
+  e.preventDefault();
+  const li = document.createElement("li");
+  li.classList.add("list-group-item");
+  li.appendChild(document.createTextNode(input.value));
+  ul.appendChild(li);
+  const btnDel = document.createElement("button");
+  btnDel.className = "btn btn-danger btn-sm float-end delete";
+  btnDel.appendChild(document.createTextNode("X"));
+  li.appendChild(btnDel);
+  input.value = "";
+});
+
+ul.addEventListener("click", (e) => {
+  if (e.target.classList.contains("delete")) {
+    var li = e.target.parentElement;
+    ul.removeChild(li);
+  }
+});
+
+filter.addEventListener("keyup", (e) => {
+  const text = e.target.value.toLowerCase();
+
+  let items = ul.getElementsByTagName("li");
+  Array.from(items).forEach((li) => {
+    const searchItem = li.firstChild.textContent;
+    if (searchItem.toLowerCase().indexOf(text) != -1) {
+      li.style.display = "block";
+    } else {
+      li.style.display = "none";
+    }
+  });
+});
